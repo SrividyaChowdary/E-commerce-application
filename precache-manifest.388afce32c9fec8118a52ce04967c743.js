f0588da1b3ba2fbb63b7a131393360162c129214
SrividyaChowdary <32638060+SrividyaChowdary@users.noreply.github.com>
2021-03-18 16:01:07 +0530

    Updates

---
+++ precache-manifest.388afce32c9fec8118a52ce04967c743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192034820f82075ee1822bfb03768853",
+    "url": "/E-commerce-application/products/index.html"
+  },
+  {
+    "revision": "1882cf71804d3e225509",
+    "url": "/E-commerce-application/products/static/css/main.494f432f.chunk.css"
+  },
+  {
+    "revision": "0f16ad93ec4e9302221a",
+    "url": "/E-commerce-application/products/static/js/2.11d69690.chunk.js"
+  },
+  {
+    "revision": "29b8e4639049a11b363af39770e80fd1",
+    "url": "/E-commerce-application/products/static/js/2.11d69690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1882cf71804d3e225509",
+    "url": "/E-commerce-application/products/static/js/main.1a92d244.chunk.js"
+  },
+  {
+    "revision": "0b34081a317cef5c3c4e",
+    "url": "/E-commerce-application/products/static/js/runtime-main.d9ee06eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/E-commerce-application/precache-manifest.1684ee8740dd4c7c729694b9b30cb233.js"
+  "/E-commerce-application/products/precache-manifest.388afce32c9fec8118a52ce04967c743.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/E-commerce-application/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/E-commerce-application/products/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
