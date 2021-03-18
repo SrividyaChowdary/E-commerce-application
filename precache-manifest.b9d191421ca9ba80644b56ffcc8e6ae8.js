8a83b14d63bc2e990d7a871c175c864ec71b420d
SrividyaChowdary <32638060+SrividyaChowdary@users.noreply.github.com>
2021-03-18 15:46:21 +0530

    Updates

---
+++ precache-manifest.b9d191421ca9ba80644b56ffcc8e6ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b86c7c5b81d29ccec96b978371b0cc",
+    "url": "/SrividyaChowdary/E-commerce-application/index.html"
+  },
+  {
+    "revision": "a975a270dfc14677607c",
+    "url": "/SrividyaChowdary/E-commerce-application/static/css/main.494f432f.chunk.css"
+  },
+  {
+    "revision": "0f16ad93ec4e9302221a",
+    "url": "/SrividyaChowdary/E-commerce-application/static/js/2.11d69690.chunk.js"
+  },
+  {
+    "revision": "29b8e4639049a11b363af39770e80fd1",
+    "url": "/SrividyaChowdary/E-commerce-application/static/js/2.11d69690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a975a270dfc14677607c",
+    "url": "/SrividyaChowdary/E-commerce-application/static/js/main.758283d9.chunk.js"
+  },
+  {
+    "revision": "54e693582e7dc1f379b0",
+    "url": "/SrividyaChowdary/E-commerce-application/static/js/runtime-main.f0738fb3.js"
+  }
+]);
